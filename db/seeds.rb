00d66cbc8e504f3875448f810a22154fa6f1686a
zach beecher <zachbeecher@gmail.com>
2017-06-30 16:13:19 -0700

    added faker gem for gameofthrones names for reviews and fake products, 250 reviews 50 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  product = Product.create!(
+    name: Faker::Commerce.product_name,
+    cost: Faker::Commerce.price,
+    country: Faker::Address.country
+  )
+  250.times do |index|
+    Review.create!(
+      author: Faker::GameOfThrones.character,
+      rating: Faker::Number.between(1, 5),
+      content: Faker::Lorem.sentence(10),
+      product_id: product.id
+    )
+  end
+end
