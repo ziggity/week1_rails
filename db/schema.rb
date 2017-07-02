5a4db6886d8d49f56b7224403325deceb5b455ae
zach beecher <zachbeecher@gmail.com>
2017-07-02 10:00:44 -0700

    add minor tweaks to review page

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630153057) do
+ActiveRecord::Schema.define(version: 20170630152423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.float "cost"
+    t.decimal "cost"
+    t.text "description"
+    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "country"
   end
 
   create_table "reviews", force: :cascade do |t|
